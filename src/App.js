a9cbdf981ec2f7f652644864d4eac820404ac4b3
sarahcheatham <sarah.cheatham14@gmail.com>
2019-01-25 20:09:39 -0600

    images set up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import trampoline from "./images/trampoline.gif";
+import llamito from "./images/llamito3.gif";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="header">Llamito</div>
+        <div className="imgBox">
+          <img alt="trampoline" src={trampoline} id="trampoline"/>
+        </div>
+        <div className="llamaBox">
+          <img alt="Llamito" src={llamito} id="llamito"/>
+        </div>
       </div>
     );
   }
