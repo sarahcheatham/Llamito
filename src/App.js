4fed34d4f2913701ee23534908ee13ed59ca4b35
sarahcheatham <sarah.cheatham14@gmail.com>
2019-02-02 19:28:14 -0600

    llama bounce working and bouncecount working

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
 import React, { Component } from 'react';
-import { bounce } from 'react-animations';
 import trampoline from "./images/trampoline.gif";
 import llamito from "./images/llamito3.gif";
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      bounce: false,
+      bounceCount: 0
+    }
+    this.mouseDown = this.mouseDown.bind(this);
+    this.mouseUp = this.mouseUp.bind(this);
+  }
+  
+  mouseDown(e){
+    e.preventDefault();
+    this.setState({bounce: !this.state.bounce, bounceCount: this.state.bounceCount+1})
+    e.target.classList.add('animated', 'bounce')
+    console.log("mouseDown",e.target.classList)
+    console.log(this.state.bounceCount)
+  }
+
+  mouseUp(e){
+    e.preventDefault();
+    e.target.classList.remove('animated', 'bounce');
+    console.log("mouseUp",e.target.classList)
+  }
+
   render() {
     return (
       <div className="App">
         <div className="header">Llamito</div>
-        <div className="imgBox">
+        <div id="animationContainer">
           <img alt="trampoline" src={trampoline} id="trampoline"/>
-        </div>
-        <div className="llamaBox">
-          <img alt="Llamito" src={llamito} id="llamito"/>
+          <img alt="Llamito" src={llamito} onMouseDown={this.mouseDown} onMouseUp={this.mouseUp} className="llamito"/>
         </div>
       </div>
     );
