930c852fcc0377b524ac38073100ea068d4d18a2
sarahcheatham <sarah.cheatham14@gmail.com>
2019-04-23 11:05:31 -0500

    Updates

---
+++ precache-manifest.6777aba90bc3217b21d01df70c469171.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "9047e8824ce606f3688e",
+    "url": "/Llamito/static/css/main.e3a3c7b5.chunk.css"
+  },
+  {
+    "revision": "9047e8824ce606f3688e",
+    "url": "/Llamito/static/js/main.9047e882.chunk.js"
+  },
+  {
+    "revision": "b744f245c56d628da0c7",
+    "url": "/Llamito/static/js/1.b744f245.chunk.js"
+  },
+  {
+    "revision": "3e22ccdc23ef32c144a4",
+    "url": "/Llamito/static/js/runtime~main.3e22ccdc.js"
+  },
+  {
+    "revision": "bde4fbba0bb0472320e8422793984d9f",
+    "url": "/Llamito/static/media/trampoline.bde4fbba.gif"
+  },
+  {
+    "revision": "e684ac059763bc8618475e7ef09ac7d7",
+    "url": "/Llamito/static/media/llamito3.e684ac05.gif"
+  },
+  {
+    "revision": "e1685a93908cf91ed2f8edf6b46bdf17",
+    "url": "/Llamito/static/media/06BirthdayCake.e1685a93.m4a"
+  },
+  {
+    "revision": "2e355066fe4f0b36276819c15fdb69af",
+    "url": "/Llamito/static/media/sick3.2e355066.m4v"
+  },
+  {
+    "revision": "fb43fff1b1437d74fa5444ffb58a2603",
+    "url": "/Llamito/static/media/sick.fb43fff1.mov"
+  },
+  {
+    "revision": "edc67104c64dccf0207ef9a02fd247e0",
+    "url": "/Llamito/index.html"
+  }
+];
